9bbf1fcfe50f4cc293df11872991df1768475e90
Ameer Muhammed <ami.mullan@gmail.com>
2020-09-08 22:01:16 +0530

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by AmeerMuhammed on 9/8/20.
+//  Copyright © 2020 AmeerMuhammed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
